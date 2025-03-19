17be147efec99fb406aa599f3a9f2acfa07b61dd
Thyale MD <thyaledantas@gmail.com>
2025-03-19 08:12:09 -0300

    Sessão tecnologia

--- BrisaStudio/src/App.jsx
+++ BrisaStudio/src/App.jsx
@@ -1,11 +1,13 @@
 import CapaPrincipal from "./Componentes/CapaPrincipal"
 import HeaderSuperior from "./Componentes/HeaderSuperior"
+import Tecnologia from "./Componentes/Tecnologia"
 
 function App() {
   return (
     <>
       <HeaderSuperior/>
       <CapaPrincipal/>
+      <Tecnologia/>
     </>
   )
 }
---
+++ BrisaStudio/src/Componentes/Tecnologia.jsx
@@ -0,0 +1,17 @@
+import ImgTecnologia from "../assets/images/ImgTecnologia.png"
+
+function Tecnologia() {
+    return (
+      <>
+        <div className="ContainerTecnologia">
+           <img src={ImgTecnologia} alt="Imagem de uma ilustração sobre a sessão de tecnologia" />
+           <h2>Tecnologia Inteligente: Viva a Experiência da Alexa</h2>
+           <p>Nosso espaço é totalmente integrado à Alexa! Controle a iluminação, o ar-condicionado e até mesmo as persianas por comandos de voz. Conforto e praticidade ao seu alcance!</p>
+           <a href="">Saiba Mais</a>
+        </div>
+      </>
+    )
+  }
+  
+  export default Tecnologia
+  
