cced880c69bc497154127be55d6150b994de0a99
Thyale MD <thyaledantas@gmail.com>
2025-03-20 11:31:00 -0300

    Atualizações

--- BrisaStudio/src/App.jsx
+++ BrisaStudio/src/App.jsx
@@ -1,15 +1,9 @@
-import AreaInterna from "./Componentes/AreaInterna"
-import CapaPrincipal from "./Componentes/CapaPrincipal"
 import HeaderSuperior from "./Componentes/HeaderSuperior"
-import Tecnologia from "./Componentes/Tecnologia"
 
 function App() {
   return (
     <>
       <HeaderSuperior/>
-      <CapaPrincipal/>
-      <Tecnologia/>
-      <AreaInterna/>
     </>
   )
 }
