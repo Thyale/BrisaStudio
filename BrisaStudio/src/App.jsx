1cada389d51da891af7e3815cb2402844d3d51db
Thyale MD <thyaledantas@gmail.com>
2025-03-20 19:24:39 -0300

    Sessão de Tecnologia pronta

--- BrisaStudio/src/App.jsx
+++ BrisaStudio/src/App.jsx
@@ -1,11 +1,13 @@
 import HeaderSuperior from "./Componentes/HeaderSuperior"
 import SessInicial from "./Componentes/SessInicial"
+import Tecnologia from "./Componentes/Tecnologia"
 
 function App() {
   return (
     <>
       <HeaderSuperior/>
       <SessInicial/>
+      <Tecnologia/>
     </>
   )
 }
