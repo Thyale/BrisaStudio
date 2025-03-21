96e967967e8116ac0e87af3c175fc0df586f99f6
Thyale MD <thyaledantas@gmail.com>
2025-03-21 10:08:52 -0300

    Área externa

--- BrisaStudio/src/Componentes/AreaExterna.jsx
+++ BrisaStudio/src/Componentes/AreaExterna.jsx
@@ -2,14 +2,27 @@ import ImgAreaExternaCapa from "../assets/images/ImgAreaExternaCapa.jpeg"
 import ImgAreaExterna1 from "../assets/images/ImgAreaExterna1.jpeg"
 import ImgAreaExterna2 from "../assets/images/ImgAreaExterna2.jpeg"
 import ImgAreaExterna3 from "../assets/images/ImgAreaExterna3.jpeg"
-import ImgAreaExterna4 from "../assets/images/ImgAreaExterna4.jpeg"
 
 function AreaExterna() {
     return (
         <div className="ContainerAreaExterna">
             <div className="ContainerAreaExterna_Textos">
+                <h2>Área de Lazer: Diversão e Relaxamento</h2>
+                <p className="ContainerAreaExterna_Textos_Subtitle">Desfrute de uma área de lazer completa, ideal para momentos inesquecíveis.</p>
+                <p className="ContainerAreaExterna_Textos_Info">(Arraste para o lado para ver mais fotos)</p>
+                <ul>
+                    <li>Área de jantar externa</li>
+                    <li>Cozinha compartilhada ao ar livre</li>
+                    <li>Cozinha compartilhada ao ar livre</li>
+                    <li>Itens básicos de praia</li>
+                    <li>Piscina relaxante</li>
+                </ul>
             </div>
             <div className="ContainerAreaExterna_Imgs">
+                <img src={ImgAreaExternaCapa} alt="Imagem da área externa" />
+                <img src={ImgAreaExterna1} alt="Imagem da área externa" />
+                <img src={ImgAreaExterna2} alt="Imagem da área externa" />
+                <img src={ImgAreaExterna3} alt="Imagem da área externa" />
             </div>
         </div>
     )
